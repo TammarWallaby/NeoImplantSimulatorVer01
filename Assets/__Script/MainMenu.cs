c469ed05f284ef0b0486896e7461ea972e8e7686
52jjoee <mg4725@naver.com>
2024-10-11 11:00:05 +0900

    11
    11

--- Assets/__Script/MainMenu.cs
+++ Assets/__Script/MainMenu.cs
@@ -35,12 +35,12 @@ public class MainMenu : MonoBehaviour
     public void IncisorGame()
     {
         // 예를 들어, 게임이 시작하는 씬이 "GameScene"이라면 해당 씬을 로드
-        SceneManager.LoadScene("Test");
+        SceneManager.LoadScene("Test 1");
     }
 
     public void MolarGame()
     {
         // 예를 들어, 게임이 시작하는 씬이 "GameScene"이라면 해당 씬을 로드
-        SceneManager.LoadScene("Test");
+        SceneManager.LoadScene("Test 2");
     }
 }
