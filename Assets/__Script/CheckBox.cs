a658a37e9997fe540400f53dec8737fa32f1f157
52jjoee <mg4725@naver.com>
2024-10-15 16:50:39 +0900

    11
    11

---
+++ Assets/__Script/CheckBox.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckBox : MonoBehaviour
+{
+    public Toggle checkBox; // 체크박스 (Toggle) 오브젝트
+    public bool conditionMet = false; // 특정 상황이 충족되었는지 여부를 저장하는 변수
+
+    private void Update()
+    {
+        // 특정 상황이 충족되었을 때 체크박스를 체크
+        if (conditionMet)
+        {
+            checkBox.isOn = true; // 체크박스를 체크 상태로 변경
+        }
+        else
+        {
+            checkBox.isOn = false; // 조건이 충족되지 않으면 체크 해제
+        }
+    }
+
+    // 특정 조건을 만족했을 때 호출될 함수 예시
+    public void MeetCondition()
+    {
+        conditionMet = true; // 조건이 충족되었음을 나타냄
+    }
+
+    // 조건이 충족되지 않았을 때 호출될 함수 예시
+    public void UnmeetCondition()
+    {
+        conditionMet = false; // 조건을 충족하지 않았음을 나타냄
+    }
+}
---
+++ Assets/__Script/FailText.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * 바꾸는 Text UI 에 위치
+ * 도구 실패 시 Fail 패널에 "00" 하지 못했습니다 라고 뜸
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FailText : MonoBehaviour
+{
+    public Text uiText; // UI 텍스트 컴포넌트
+
+    private void Start()
+    {
+        if (uiText != null)
+        {
+            uiText.text = "시작 텍스트"; // 초기 텍스트 설정
+        }
+
+        /* 특정 상황 입력
+        if ()
+        {
+        특정 상황 (() => ChangeText("새로운 텍스트!"));
+        }
+        */
+
+        //예시
+
+    }
+
+    // 텍스트를 변경하는 메서드
+    public void ChangeText(string newText)
+    {
+        if (uiText != null)
+        {
+            uiText.text = newText; // 새로운 텍스트로 변경
+        }
+    }
+}
