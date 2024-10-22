f797975492ae385871ed52061547cb792bcc66a0
52jjoee <mg4725@naver.com>
2024-10-22 10:46:31 +0900

    Revert "Merge branch 'TMW'"
    This reverts commit cba8c1dd9273303bcd217cac954f04d7291ba76e, reversing
    changes made to bda030abb144d8e4179d3dbcd39ba402fac127f7.

--- Assets/__Script/InGame/SurgeryCamController.cs
+++ Assets/__Script/InGame/SurgeryCamController.cs
@@ -355,7 +355,7 @@ public class SurgeryCamController : MonoBehaviour
                                     }
                                     break;
                                 case SurgeryState.HealingAbutmentRemove:
-                                    if(heldTool.name=="ToolDriver")
+                                    if(heldTool.name=="ToolTorqueRatchet") // 드라이버 교체예정
                                     {
                                         this.enabled = false;
                                         Destroy(healingAbutment);
@@ -369,21 +369,14 @@ public class SurgeryCamController : MonoBehaviour
                                     }
                                     break;
                                 case SurgeryState.AbutmentPlace:
-                                    if (heldTool.name == "ToolDriver" && heldAbutment != null)
+                                    if (heldTool.name == "ToolAbutment") // 드라이버 + 어버 교체예정
                                     {
-                                        if (heldAbutment.name == "ToolAbutment")
-                                        {
-                                            this.enabled = false;
-                                            Destroy(heldAbutment);
-                                            abutment.SetActive(true);
-                                            this.enabled = true;
-                                            currentState = SurgeryState.CrownPlace;
-                                            Debug.Log("성공!");
-                                        }
-                                        else
-                                        {
-                                            Debug.Log("어버 틀림 ㅋ");
-                                        }
+                                        this.enabled = false;
+                                        Destroy(heldTool);
+                                        abutment.SetActive(true);
+                                        this.enabled = true;
+                                        currentState = SurgeryState.CrownPlace;
+                                        Debug.Log("성공!");
                                     }
                                     else
                                     {
@@ -391,7 +384,7 @@ public class SurgeryCamController : MonoBehaviour
                                     }
                                     break;
                                 case SurgeryState.CrownPlace:
-                                    if(heldTool.name=="ToolIncisorCrown"||heldTool.name=="ToolMolarCrown") 
+                                    if(heldTool.name=="ToolIncisorCrown") // 이름때문에 어금니 X
                                     {
                                         this.enabled = false;
                                         Destroy(heldTool);
